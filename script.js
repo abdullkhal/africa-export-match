7730bb90458487cac962e0ade1d91e874c0c5f6f
abdullkhal <ab.abdull87@gmail.com>
2025-08-08 23:11:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-
-// FAQ toggle
-document.querySelectorAll('.faq-q').forEach(btn => {
-  btn.addEventListener('click', () => btn.parentElement.parentElement.classList.toggle('open'));
-});
+// Placeholder for any JS
